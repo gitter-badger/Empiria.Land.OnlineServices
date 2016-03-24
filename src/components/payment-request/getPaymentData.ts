813cd1bfd6b2c8bf9fa5d6d25f4246b0b9452499
hugoage <hgarcia@ontica.org>
2016-03-24 11:51:01 -0600

    PAgos alfa 1

--- src/components/payment-request/getPaymentData.ts
+++ src/components/payment-request/getPaymentData.ts
@@ -1,16 +1,16 @@
 import {Component, ViewChild} from 'angular2/core';
-// import {OnlineServices, PaymentRequest, OnlineTransaction} from 'empiria-land/online.services';
+
 import {MessageBox} from '../windows/message-box';
 
-enum PayMethod {card = 0 , window = 1, store = 2, unknown = 99 };
+enum PayMethod { card = 0, window = 1, store = 2, unknown = 99 };
 
 @Component({
-   selector: 'get-payment-data',
-   templateUrl: './components/payment-request/getPaymentData.html' ,
-   directives: [MessageBox]
+  selector: 'get-payment-data',
+  templateUrl: './components/payment-request/getPaymentData.html',
+  directives: [MessageBox]
 })
 export class GetPaymentDataCmp {
-    // public paymentRequest: PaymentRequest = this.getNewPendingNoteRequest();
+
   @ViewChild(MessageBox) public messageBox: MessageBox;
 
   public paymentMethod: string = "unknown";
@@ -23,33 +23,33 @@ export class GetPaymentDataCmp {
   public validationCode: string = "";
   public payMode: string = "";
 
-public showFields(value: string): void {
-       try {
-                this.cleanPayForm();
-                this.paymentMethod = PayMethod[PayMethod[value]];
-        } catch ( e ) {
-                this.messageBox.showException( e );
-            }
-}
-public cleanPayForm(): void {
-        try {
-                this.payPlace = "";
-                this.bankName = "";
-                this.cardNumber = "";
-                this.name = "";
-                this.month = "";
-                this.year = "";
-                this.validationCode = "";
-            } catch ( e ) {
-                    this.messageBox.showException( e );
-            }
+  public showFields(value: string): void {
+    try {
+      this.cleanPayForm();
+      this.paymentMethod = PayMethod[PayMethod[value]];
+    } catch (e) {
+      this.messageBox.showException(e);
     }
-public createPayment(): void {
-try {
-        this.messageBox.showMessage("Procesando el pago......");
-} catch ( e ) {
-        this.messageBox.showException( e );
+  }
+  public cleanPayForm(): void {
+    try {
+      this.payPlace = "";
+      this.bankName = "";
+      this.cardNumber = "";
+      this.name = "";
+      this.month = "";
+      this.year = "";
+      this.validationCode = "";
+    } catch (e) {
+      this.messageBox.showException(e);
     }
-}
+  }
+  public createPayment(): void {
+    try {
+      this.messageBox.showMessage("Procesando el pago......");
+    } catch (e) {
+      this.messageBox.showException(e);
+    }
+  }
 
 }
--- src/components/pending-note-request/getPendingNoteData.ts
+++ src/components/pending-note-request/getPendingNoteData.ts
@@ -4,66 +4,65 @@ import {OnlineServices, PendingNoteRequest, OnlineTransaction} from 'empiria-lan
 import {MessageBox} from '../windows/message-box';
 
 @Component({
-   selector: 'get-pending-note-data',
-   templateUrl: './components/pending-note-request/get-pending-note-data.html' ,
-   directives: [MessageBox]
+  selector: 'get-pending-note-data',
+  templateUrl: './components/pending-note-request/get-pending-note-data.html',
+  directives: [MessageBox]
 })
 export class GetPendingNoteDataCmp {
 
   public pendingNoteRequest: PendingNoteRequest = this.getNewPendingNoteRequest();
   @ViewChild(MessageBox) public messageBox: MessageBox;
 
-public searchProperty(): void {
-try {
-        let exists = OnlineServices.existsProperty(this.pendingNoteRequest.realPropertyUID);
-            if (exists === true) {
-            this.messageBox.showMessage("!PREDIO ENCONTRADO¡");
-                } else {
-            this.messageBox.showMessage("EL PREDIO NO ES VALIDO...");
-             }
-         exists = false;
-} catch ( e ) {
-        this.messageBox.showException( e );
+  public searchProperty(): void {
+    try {
+      let exists = OnlineServices.existsProperty(this.pendingNoteRequest.realPropertyUID);
+      if (exists === true) {
+        this.messageBox.showMessage("!PREDIO ENCONTRADO¡");
+      } else {
+        this.messageBox.showMessage("EL PREDIO NO ES VALIDO...");
+      }
+      exists = false;
+    } catch (e) {
+      this.messageBox.showException(e);
     }
-}
+  }
 
-public createPreliminaryNotice(): void {
-try {
-        let onlineTransaction = this.getOnlineTransaction(this.pendingNoteRequest);
-        this.messageBox.showMessage("Creando aviso preventivo...: " + onlineTransaction);
-        this.showOnlineTransactionMessage(onlineTransaction);
-} catch ( e ) {
-        this.messageBox.showException( e );
+  public createPreliminaryNotice(): void {
+    try {
+      let onlineTransaction = this.getOnlineTransaction(this.pendingNoteRequest);
+      this.messageBox.showMessage("Creando aviso preventivo...: " + onlineTransaction);
+      this.showOnlineTransactionMessage(onlineTransaction);
+    } catch (e) {
+      this.messageBox.showException(e);
     }
-}
+  }
 
-private getOnlineTransaction(pendingNoteRequest: PendingNoteRequest): OnlineTransaction {
-        return OnlineServices.requestPendingNoteRecording(pendingNoteRequest);
-    }
+  private getOnlineTransaction(pendingNoteRequest: PendingNoteRequest): OnlineTransaction {
+    return OnlineServices.requestPendingNoteRecording(pendingNoteRequest);
+  }
 
-private getNewPendingNoteRequest(): PendingNoteRequest {
-        let pendingNoteRequest: PendingNoteRequest = {
-            realPropertyUID :  '',
-            externalTransactionNo : (Math.random() * 1000000).toFixed(0),
-            externalTransactionTime : new Date(2016, 1, 16, 14, 10, 34),
-            paymentAmount : 120.00,
-            paymentReceiptNo : "A-89078-2016." +  (Math.random() * 1000).toFixed(0),
-            requestedBy :  '',
-            notaryId : 73,
-            projectedActId : 0,
-            projectedOwner : ''
-        };
-        return pendingNoteRequest;
-}
-
-private showOnlineTransactionMessage(onlineTransaction: OnlineTransaction): void {
-        let transationRequest = "Número de Transacción: " + onlineTransaction.externalTransactionNo + "\n" +
-            "Fecha de presentación: " + onlineTransaction.presentationTime + "\n" +
-            "Folio real:            " + onlineTransaction.realPropertyUID + "\n" +
-            "Solicitado por:        " + onlineTransaction.requestedBy + "\n" +
-            "Status atcual:         " + onlineTransaction.status + "\n" +
-            "Uid:                   " + onlineTransaction.uid;
-        this.messageBox.showMessage("La solicitud de aviso preventivo se creó satistactoriamente. \n" +
-                                            transationRequest);
-        }
+  private getNewPendingNoteRequest(): PendingNoteRequest {
+    let pendingNoteRequest: PendingNoteRequest = {
+      realPropertyUID: '',
+      externalTransactionNo: (Math.random() * 1000000).toFixed(0),
+      externalTransactionTime: new Date(2016, 1, 16, 14, 10, 34),
+      paymentAmount: 120.00,
+      paymentReceiptNo: "A-89078-2016." + (Math.random() * 1000).toFixed(0),
+      requestedBy: '',
+      notaryId: 73,
+      projectedActId: 0,
+      projectedOwner: ''
+    };
+    return pendingNoteRequest;
+  }
+  private showOnlineTransactionMessage(onlineTransaction: OnlineTransaction): void {
+    let transationRequest = "Número de Transacción: " + onlineTransaction.externalTransactionNo + "\n" +
+      "Fecha de presentación: " + onlineTransaction.presentationTime + "\n" +
+      "Folio real:            " + onlineTransaction.realPropertyUID + "\n" +
+      "Solicitado por:        " + onlineTransaction.requestedBy + "\n" +
+      "Status atcual:         " + onlineTransaction.status + "\n" +
+      "Uid:                   " + onlineTransaction.uid;
+    this.messageBox.showMessage("La solicitud de aviso preventivo se creó satistactoriamente. \n" +
+      transationRequest);
+  }
 }
